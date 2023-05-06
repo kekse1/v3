c7765f909e206d4d8dd9b0a622f6e2fafffba4cb
Sebastian Kucharczyk <kuchen@kekse.biz>
2023-05-06 23:08:28 +0200

    Saturday, 2023-05-06 (23:08:26)

--- scripts/tree.sh
+++ scripts/tree.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
+
+if [[ ! -d ~/git/hardware/ ]]; then
+	echo " >> No '/git/hardware/' directory found." >&2
+	exit 1
+fi
+
 dirname="$(realpath ./)"
 cd ~/git/
 tree -d --charset=UTF-8 --nolinks --noreport -o "$dirname/main.txt" -- hardware/
 tree -d --charset=UTF-8 --nolinks --noreport -o "$dirname/main.xml" -X -- hardware/
 tree -d --charset=UTF-8 --nolinks --noreport -o "$dirname/main.json" -J -- hardware/
+
