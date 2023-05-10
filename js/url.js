495b84128f11049a3cd265cb535e3ad286b489b2
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2023-05-10 05:29:16 +0200

    Mittwoch, 2023-05-10 (05:29:14)

---
+++ js/url.js
@@ -0,0 +1,15 @@
+(function()
+{
+
+	//
+	//TODO/.render() for <a-uniform>.. ^_^
+	//
+	Object.defineProperty(URL.prototype, 'render', { value: function(_anchor, _brackets, _arrows)
+	{
+throw new Error('TODO');//a-uniform...
+	}});
+
+	//
+	
+})();
+
