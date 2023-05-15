b5afa0fbe0dbe8e362113d8aa203bcfff53b0f28
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2023-05-15 17:28:07 +0200

    Montag, 2023-05-15 (17:28:05)

--- scripts/tree.sh
+++ scripts/tree.sh
@@ -7,7 +7,8 @@ fi
 
 dirname="$(realpath ./)"
 cd ~/git/
-tree -d --charset=UTF-8 --nolinks --noreport -- hardware/ >"$dirname/main.txt"
-tree -d --charset=UTF-8 --nolinks --noreport -X -- hardware/ >"$dirname/main.xml"
-tree -d --charset=UTF-8 --nolinks --noreport -J -- hardware/ >"$dirname/main.json"
+
+tree -d --charset=UTF-8 --nolinks --noreport -- hardware/ >"${dirname}/main.txt"
+tree -d --charset=UTF-8 --nolinks --noreport -X -- hardware/ >"${dirname}/main.xml"
+tree -d --charset=UTF-8 --nolinks --noreport -J -- hardware/ >"${dirname}/main.json"
 
