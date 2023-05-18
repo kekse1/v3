014d9b52b783a1d82da66b7a14f628487ce79575
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2023-05-19 01:04:33 +0200

    Freitag, 2023-05-19 (01:04:31)

--- js.ORIG/area.js
+++
@@ -1,8 +0,0 @@
-if(!customElements.get('a-area')) customElements.define('a-area', Area = HTMLAreaElement = class HTMLAreaElement extends HTMLElement
-{
-	constructor(... _args)
-	{
-		super(... _args);
-	}
-});
-
--- js.ORIG/sort.js
+++
@@ -1,2 +0,0 @@
-// http://codercorner.com/RadixSortRevisited.htm
-//
